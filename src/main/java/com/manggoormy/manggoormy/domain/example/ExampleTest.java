c5212509c2feb0efc723eb1f38d7e2aab0652169
hwan2da <dla612@naver.com>
2023-03-30 21:30:41 +0900

    fix: stone entity 변경

--- src/main/java/com/manggoormy/manggoormy/domain/example/ExampleTest.java
+++ src/main/java/com/manggoormy/manggoormy/domain/example/ExampleTest.java
@@ -1,8 +1,13 @@
 package com.manggoormy.manggoormy.domain.example;
 
+import com.manggoormy.manggoormy.domain.common.dto.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleTest {
-
+    @GetMapping("/api/test")
+    public ApiResponse<String> testApi() {
+        return ApiResponse.OK;
+    }
 }
