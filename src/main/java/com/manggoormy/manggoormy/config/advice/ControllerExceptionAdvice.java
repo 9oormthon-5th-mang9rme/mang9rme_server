5baa1543360412b546674380de406b8490e8f8eb
Torres-09 <dla612@naver.com>
2023-09-19 15:43:00 +0900

    fix: 프로젝트 복구

--- src/main/java/com/manggoormy/manggoormy/config/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/manggoormy/manggoormy/config/advice/ControllerExceptionAdvice.java
@@ -3,10 +3,11 @@ package com.manggoormy.manggoormy.config.advice;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestControllerAdvice
-public class ControllerExceptionAdvice {
+public class ControllerExceptionAdvice extends ResponseEntityExceptionHandler {
 
 }
